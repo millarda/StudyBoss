0426366e6c3181401399d0940b8da1c42e61778a
Brandon Wong <bmw_fire@dhcp-168-105-242-234.wireless.manoa.hawaii.edu>
2017-09-26 17:27:07 -1000

    Initial Commit

---
+++ StudyBoss/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StudyBoss
+//
+//  Created by Brandon Wong on 9/26/17.
+//  Copyright © 2017 BMW Fire. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
